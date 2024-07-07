d35405e348eedf9d446dd9a334009bba0466553d
unknown <ricardodonado1105@gmail.com>
2024-07-06 19:27:43 -0500

    mark all as read button scripted

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-console.log("hello world!")
+const markAllButton = document.getElementsByClassName("readAll")[0]
+const notificationElements = [...document.getElementsByClassName("notification")]
+const notificationNumber = document.getElementById("notificationNumber")
+
+
+const unreadedNotis = notificationElements.filter(checkNotis)
+function checkNotis(value){
+    return value.classList.contains("unread")
+}
+
+markAllButton.addEventListener("click",()=>{
+    notificationElements.forEach(notification => {
+        if(notification.classList.contains("unread")){
+            notification.classList.remove("unread")
+        }
+    });
+    const unreadedNotis = notificationElements.filter(checkNotis)
+    notificationNumber.innerHTML = unreadedNotis.length
+})
+notificationNumber.innerHTML = unreadedNotis.length
